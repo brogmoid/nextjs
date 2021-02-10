72ebe33eb69c2a1c547dcdb4ff089f1dc29ead2e
brogmoid <78865907+brogmoid@users.noreply.github.com>
2021-02-10 16:44:29 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from 'https://www.w3schools.com/w3css/4/w3.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    <html><head></head><body><div>Website</div></body></html>
+    <html>
+      <head>
+        </head>
+        <body>
+        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+  <div class="w3-display-middle" style="white-space:nowrap;">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">GRANT <span class="w3-hide-small">McDANIEL</span> | COMPUTER PERSON</span>
+  </div>
+</div>
+          </body>
+          </html>
     </div>
   )
 }
