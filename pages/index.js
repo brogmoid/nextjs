f719889f02ac99df08b0ff8d4d75a3417fe51e39
brogmoid <78865907+brogmoid@users.noreply.github.com>
2021-02-10 16:54:57 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
-import Head from 'next/head'
+import React, { Component } from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-    <html>
-      <head>
-        </head>
-        <body>
-        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
-  <div class="w3-display-middle" style="white-space:nowrap;">
-    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">GRANT <span class="w3-hide-small">McDANIEL</span> | COMPUTER PERSON</span>
-  </div>
-</div>
-          </body>
-          </html>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wello Horld</h1>
+      </div>
+    );
+  }
 }
+
+export default App;
