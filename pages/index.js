ed5244c47123df09aad60b89e149560ffa19bb97
brogmoid <78865907+brogmoid@users.noreply.github.com>
2021-02-10 16:24:34 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-<html><head></head><body><div>Website</div></body></html>
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <html><head></head><body><div>Website</div></body></html>
+    </div>
+  )
+}
